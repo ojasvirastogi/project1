b9cf89e2c47103e5cd4b30940a13d8978de6f85f
ojasvirastogi <ojasvirastogi3@gmail.com>
2026-05-07 20:48:19 +0530

    Add homepage section image and font refresh

--- includes/header.php
+++ includes/header.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?= e($pageTitle ?? 'JobYaari Blogs') ?></title>
-    <link rel="stylesheet" href="assets/css/style.css">
+    <link rel="stylesheet" href="assets/css/style.css?v=site-bg-font-1">
     <script src="https://code.jquery.com/jquery-3.7.1.min.js"></script>
 </head>
 <body>
